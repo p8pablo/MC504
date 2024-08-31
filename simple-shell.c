02404f6ef7e54f34bd455d9e13638f0683638302
lipecorradini <luizfelipecorradini@gmail.com>
2024-08-31 16:14:22 -0300

    funcional em loop funcionando, so faltando o exit

--- simple-shell.c
+++ simple-shell.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #define MAX_PATHS 1024
 #define MAX_COMMAND_LENGTH 1024
 
@@ -37,6 +38,9 @@ char *find_command(char **directories, char *command)
     return NULL; // Comando não achado!
 }
 
+// int get_command(char *directories[MAX_PATHS], ){
+
+// }
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -48,44 +52,54 @@ int main(int argc, char *argv[])
     char *directories[MAX_PATHS];
     parse_directories(argv[1], directories);
 
-    char command_line[MAX_COMMAND_LENGTH];
-    printf("simple-shell$: ");
+    while(1){
+        int id = fork();
+        wait(NULL);
+        if(id == 0){
+            char command_line[MAX_COMMAND_LENGTH];
+            printf("simple-shell$: ");
 
-    // Recebe os comandos
-    if (fgets(command_line, sizeof(command_line), stdin) == NULL)
-    {
-        printf("fgets failed");
-        return 1;
-    }
+            // Recebe os comandos
+            if (fgets(command_line, sizeof(command_line), stdin) == NULL)
+            {
+                printf("fgets failed");
+                return 1;
+            }
 
-    command_line[strcspn(command_line, "\n")] = '\0';
+            command_line[strcspn(command_line, "\n")] = '\0';
 
-    // Separa comando e argumentos
-    char *command = strtok(command_line, " ");
-    char *args[MAX_PATHS];
-    int i = 0;
-    while (command != NULL)
-    {
-        args[i++] = command;
-        command = strtok(NULL, " ");
-    }
-    args[i] = NULL;
+            // Separa comando e argumentos
+            char *command = strtok(command_line, " ");
+            char *args[MAX_PATHS];
+            int i = 0;
+            while (command != NULL)
+            {
+                args[i++] = command;
+                command = strtok(NULL, " ");
+            }
+            args[i] = NULL;
 
-    // Procura o comando nas pastas dadas inicialmente e executa caso achado
-    char *command_path = find_command(directories, args[0]);
-    if (command_path != NULL)
-    {
-        if (execv(command_path, args) == -1)
-        {
-            printf("execv failed");
-            return 1;
+            // Procura o comando nas pastas dadas inicialmente e executa caso achado
+            char *command_path = find_command(directories, args[0]);
+            
+            if (strcmp(command_path, "exit")!= 0)
+            {
+                
+                if (execv(command_path, args) == -1)
+                {
+                    printf("execv failed");
+                    return 1;
+                }
+            }
+            else
+            {
+                exit(0);
+            }
         }
+        
     }
-    else
-    {
-        printf("Please insert command\n");
-        return 1;
-    }
+    
 
+    
     return 0;
 }
