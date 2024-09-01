d39ad59feff03c518a19495841172ddcd32ee382
lipecorradini <luizfelipecorradini@gmail.com>
2024-08-31 23:11:32 -0300

    adicionando comentarios

--- simple-shell.c
+++ simple-shell.c
@@ -11,8 +11,8 @@
 #define MAX_PATHS 1024
 #define MAX_COMMAND_LENGTH 1024
 
-// Função para dividir os diretorios da entrada
 void parse_directories(char *path, char **directories)
+// Função para dividir os diretorios da entrada
 {
     int i = 0;
     char *dir = strtok(path, ":");
@@ -25,6 +25,7 @@ void parse_directories(char *path, char **directories)
 }
 
 char *find_command(char **directories, char *command)
+// Função que itera pelos diretórios passados como parâmetro, procurando o comando desejado
 {
     static char path[MAX_COMMAND_LENGTH];
     for (int i = 0; directories[i] != NULL; i++)
@@ -38,64 +39,75 @@ char *find_command(char **directories, char *command)
     return NULL; // Comando não achado!
 }
 
-// int get_command(char *directories[MAX_PATHS], ){
 
-// }
 int main(int argc, char *argv[])
 {
+    // Se tiver menos que 2 argumentos, então os diretórios não foram passados
     if (argc != 2)
     {
         printf("Please insert directories\n");
         return 1;
     }
 
+    // Separando os diretórios
     char *directories[MAX_PATHS];
     parse_directories(argv[1], directories);
 
+    // Loop do terminal, para que só termine com o exit
     while(1){
-        int id = fork();
-        wait(NULL);
-        if(id == 0){
-            char command_line[MAX_COMMAND_LENGTH];
-            printf("simple-shell$: ");
+        
+        // Mostrando o shell e definindo a linha de comando
+        char command_line[MAX_COMMAND_LENGTH];
+        printf("simple-shell$: ");
 
-            // Recebe os comandos
-            if (fgets(command_line, sizeof(command_line), stdin) == NULL)
+        // Recebe os comandos
+        if (fgets(command_line, sizeof(command_line), stdin) == NULL)
+        {
+            printf("fgets failed");
+            return 1;
+        }
+
+        // Tirando o '\n' da linha de comando
+        command_line[strcspn(command_line, "\n")] = '\0';
+
+        // Separa comando e argumentos
+        char *command = strtok(command_line, " ");
+        char *args[MAX_PATHS];
+        int i = 0;
+        
+        // Itera pelos comandos passados, e coloca em "args"
+        while (command != NULL)
+        {
+            args[i++] = command;
+            command = strtok(NULL, " ");
+        }
+
+        // Seta o último args como null
+        args[i] = NULL;
+
+        // Procura o comando nas pastas dadas inicialmente, retorna PATH se achar e NULL se não
+        char *command_path = find_command(directories, args[0]);
+
+        // Se o comando não for exit (essa lógica tem que ser alterada)
+        //      A ideia aqui é, caso seja != de exit, se cria o fork
+        //      Porém, só o filho será executado, e o pai passará para a próxima alteração do while TRUE
+        if (strcmp(args[0],"exit") != 0)
+        {
+            int id = fork();
+            wait(NULL); // pai espera o filho acabar pra rodar
+            
+            if (execv(command_path, args) == -1)
             {
-                printf("fgets failed");
+                printf("execv failed");
                 return 1;
             }
-
-            command_line[strcspn(command_line, "\n")] = '\0';
-
-            // Separa comando e argumentos
-            char *command = strtok(command_line, " ");
-            char *args[MAX_PATHS];
-            int i = 0;
-            while (command != NULL)
-            {
-                args[i++] = command;
-                command = strtok(NULL, " ");
-            }
-            args[i] = NULL;
-
-            // Procura o comando nas pastas dadas inicialmente e executa caso achado
-            char *command_path = find_command(directories, args[0]);
-            
-            if (strcmp(command_path, "exit")!= 0)
-            {
-                
-                if (execv(command_path, args) == -1)
-                {
-                    printf("execv failed");
-                    return 1;
-                }
-            }
-            else
-            {
-                exit(0);
-            }
         }
+        else
+        {
+            break;
+        
+        }
+        exit(0);
         
     }
     
