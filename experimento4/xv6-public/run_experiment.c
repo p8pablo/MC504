0a7d7c4d01de8f7d3895c548e5ea21f86502b65b
Pablo <p8pablo@gmail.com>
2024-10-26 07:41:26 -0300

    io_bound

--- experimento4/xv6-public/run_experiment.c
+++ experimento4/xv6-public/run_experiment.c
@@ -2,11 +2,15 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_VERTICES 201
 #define INF 1e9 // Representing infinite distance
-
+#define LINE_LENGTH 100
+#define MAX_LINES 100
 #define RAND_MAX_32 ((1U << 30) - 1)
 int rseed = 1;
 
@@ -17,20 +21,22 @@ struct Graph
 };
 
 // Function to generate a pseudo-random number
-int rand() {
+int rand()
+{
     return (rseed = (rseed * 214013 + 2531011) & RAND_MAX_32) >> 15;
 }
 
 // Function to adapt random number generated into defined range
-int randomrange(int lo, int hi){
-  
+int randomrange(int lo, int hi)
+{
+
     int range = hi - lo + 1;
     return (rand() % (range) + lo);
 }
 
 // Generate random graph
 struct Graph *generate_random_graph(int num_vertices)
-{   
+{
 
     // Allocating space
     struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
@@ -50,7 +56,8 @@ struct Graph *generate_random_graph(int num_vertices)
 
     // Adding num_edges new edges to the graph
     int j = 0;
-    while(j < num_edges){
+    while (j < num_edges)
+    {
 
         // Generate two random vertices to be connected
         int v1 = randomrange(0, num_vertices - 1);
@@ -58,9 +65,10 @@ struct Graph *generate_random_graph(int num_vertices)
 
         // printf(1, "%d -> %d\n", v1, v2);
         // Assuring the edge does not connect a node to itself
-        if(v1 != v2){
+        if (v1 != v2)
+        {
             graph->adjacency_matrix[v1][v2] = 1;
-            j ++;
+            j++;
         }
     }
 
@@ -68,10 +76,13 @@ struct Graph *generate_random_graph(int num_vertices)
 }
 
 // Minimum distance to be used for Dijkstra
-int minDistance(int dist[], int sptSet[], int num_vertices) {
+int minDistance(int dist[], int sptSet[], int num_vertices)
+{
     int min = INF, min_index = -1;
-    for (int v = 0; v < num_vertices; v++) {
-        if (sptSet[v] == 0 && dist[v] <= min) {
+    for (int v = 0; v < num_vertices; v++)
+    {
+        if (sptSet[v] == 0 && dist[v] <= min)
+        {
             min = dist[v];
             min_index = v;
         }
@@ -87,24 +98,56 @@ void dijkstra(struct Graph *graph, int start_vertex)
     int num_vertices = graph->num_vertices;
     int sptSet[MAX_VERTICES];
 
-    for (int i = 0; i < num_vertices; i++) {
+    for (int i = 0; i < num_vertices; i++)
+    {
         dist[i] = INF;
         sptSet[i] = 0;
     }
 
     dist[start_vertex] = 0;
 
-    for (int count = 0; count < num_vertices - 1; count++) {
+    for (int count = 0; count < num_vertices - 1; count++)
+    {
         int u = minDistance(dist, sptSet, num_vertices);
         sptSet[u] = 1;
 
-        for (int v = 0; v < num_vertices; v++) {
+        for (int v = 0; v < num_vertices; v++)
+        {
             if (!sptSet[v] && graph->adjacency_matrix[u][v] &&
-                dist[u] != INF && dist[u] + graph->adjacency_matrix[u][v] < dist[v]) {
+                dist[u] != INF && dist[u] + graph->adjacency_matrix[u][v] < dist[v])
+            {
                 dist[v] = dist[u] + graph->adjacency_matrix[u][v];
             }
         }
     }
+}
+
+// Function to generate a random string of characters
+void generate_random_string(char *str, int length)
+{
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (int i = 0; i < length - 1; i++)
+    {
+        int key = rand() % (sizeof(charset) - 1);
+        str[i] = charset[key];
+    }
+    str[length - 1] = '\0'; // Null-terminate the string
+}
+
+// Function to write a random line to the file
+void write_random_line_to_file(const char *filename)
+{
+    FILE *file = fopen(filename, "a"); // Open file in append mode
+    if (file == NULL)
+    {
+        perror("Could not open file");
+        return;
+    }
+
+    char line[LINE_LENGTH];
+    generate_random_string(line, LINE_LENGTH); // Generate random string
+    fprintf(file, "%s\n", line);               // Write the string to the file
+    fclose(file);                              // Close the file
 }
 
 void cpu_bound_task()
@@ -114,7 +157,7 @@ void cpu_bound_task()
 
     // Generate a random graph
     struct Graph *graph = generate_random_graph(num_vertices);
-    
+
     int start_vertex = 0;
 
     // Run Dijkstra's algorithm
@@ -128,11 +171,11 @@ void io_bound_task()
     // Write 100 random lines to file
     for (int i = 0; i < 100; i++)
     {
-        // write_random_line_to_file(filename); // TODO
+        write_random_line_to_file(filename); // TODO
     }
 
     // Permute lines
-    // permute_lines(filename); // TODO
+    permute_lines(filename); // TODO
 
     // Delete file
     unlink(filename);
@@ -145,7 +188,7 @@ void run_experiment(int cpu_count, int io_count)
         if (fork() == 0)
         {
             cpu_bound_task();
-            exit();
+            exit(0);
         }
     }
 
@@ -154,7 +197,7 @@ void run_experiment(int cpu_count, int io_count)
         if (fork() == 0)
         {
             io_bound_task();
-            exit();
+            exit(0);
         }
     }
 
@@ -171,7 +214,7 @@ int main(int argc, char *argv[])
 {
     printf(1, "My first xv6 program learnt at GFG\n");
     run_experiment(1, 1);
-    exit();
+    exit(0);
 }
 
 // This code is contributed by sambhav228
